c0122c4ae306b2d9c3e9e017f1f08b531033e9ff
designervoid <newluciocheg@gmail.com>
2019-12-21 14:23:52 +0300

    remove vuex and router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <MainView/>
-    <router-view/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 import Buefy from "buefy";
 
 import "./main.scss";
@@ -21,7 +19,5 @@ Vue.use(Buefy, {
 });
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
